70418fe9c8f600abb3be462573ab49a134599188
or_aviv <orab48@gmail.com>
2019-02-05 10:37:23 +0200

    changes

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main() {
     printf("Hello! the matrix size is: %d \n", N);
     printf("Please insert matrix values in the following form: \n"
            "\"x y\" to set the value in [x][y] to 1; \n"
-           "When you finished, insert -\"-1 -1\" to continue;");
+           "When you finished, insert -\"-1 -1\" to continue;\n");
 
     while (scanf("%d %d", &row, &column) != EOF)
     {
@@ -19,6 +19,21 @@ int main() {
             break;
         /*as instructed - assuming that input values are valid*/
         my_matrix[row][column] = 1;
+    }
+    print_my_matrix(my_matrix);
+
+    printf("Please enter index's to validate if the index's value is TRUE or FAlSE;\n"
+           "if you finished, enter - \"-1 -1\" as so to continue. \n");
+    while (scanf("%d %d", &row, &column) != EOF)
+    {
+        if (row == -1 && column == -1)
+            break;
+
+        if (path(my_matrix, row, column) == TRUE)
+            printf("value in: matrix[%d][%d] is %d \n",row, column, TRUE);
+        else
+            printf("value in: matrix[%d][%d] is %d \n",row, column, FALSE);
+
     }
 
 
